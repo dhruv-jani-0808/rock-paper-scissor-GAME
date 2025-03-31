52239da6a4c0007d64125c68e8a8cedc0574ae76
jani dhruv <24cp042@bvmengineering.ac.in>
2025-03-31 14:20:55 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,52 @@
+//logic of stone paper scissor
+let userAns;
+//selecting the elements
+let userScore = document.querySelector("#userScore");
+let compScore = document.querySelector("#compScore");
+
+let rock = document.querySelector("#rock");
+let paper = document.querySelector("#paper");
+let scissor = document.querySelector("#scissor");
+
+//assigning the values of userAns
+rock.addEventListener("click", () => {
+    userAns = 0;
+    playgame();
+})
+paper.addEventListener("click", () => {
+    userAns = 1;
+    playgame();
+})
+scissor.addEventListener("click", () => {
+    userAns = 2;
+    playgame();
+})
+
+//our function which will match both the result 
+function playgame() {
+    let result = document.querySelector("#result");
+    let random = Math.floor(Math.random() * 3);
+
+    let img = document.querySelector("#comp-image");
+    if (random === 0) {
+        img.innerHTML = "<img src='rock.jpeg'>";
+    }
+    else if (random === 1) {
+        img.innerHTML = "<img src='paper.jpeg'>";
+    }
+    else {
+        img.innerHTML = "<img src='scissor.jpeg'>";
+    }
+
+    if (random === userAns) {
+       result.innerText = "it's a draw!";
+    }
+    else if((random === 0 && userAns === 2) || (random === 1 && userAns === 0) || (random === 2 && userAns === 1)) {
+        result.innerText = "The winner is computer!";
+        compScore.innerText++;
+    }
+    else {
+        result.innerText = "The winner is user!";
+        userScore.innerText++;
+    }
+}
